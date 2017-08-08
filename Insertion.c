affc042724e58a586fae2015f25685adcb708751
mv9944 <mv9944@gmail.com>
2017-08-08 16:43:14 +0300

    Update Insertion.c

--- Insertion.c
+++ Insertion.c
@@ -8,7 +8,50 @@
 /*Function that insert the symbol into the symbols tabel by the type: 16 or 17 or 18 is data;19 is .entry and 20 is .extern otherwise is instruction type. and update SC counter too*/
 void insertSymbolToTable(char *data,int type)
 {
-    
+        Symbol* temp;
+    Symbol ** temp1;
+    temp = (Symbol*)malloc(sizeof(Symbol));
+    if (!temp){
+
+        printf("Error in allocating memory for Symbol_table");
+        printf("\n program will terminate");
+        exit(0);
+
+    }
+    temp->type = type;
+    temp->label_name = data;
+    int i = 1;
+
+    if (!symbol_table) {
+
+        symbol_table = (Symbol **)malloc(sizeof(Symbol)*(i+1));
+        if (!symbol_table){
+
+            printf("Error in allocating memory for Symbol_table");
+            printf("\n program will terminate");
+            exit(0);
+
+        }
+        symbol_table[0] = temp;
+        free(temp);
+
+    }
+
+    else
+    {
+        i = sizeof(symbol_table/ sizeof(Symbol));
+       temp1 = (Symbol **)realloc(symbol_table,i+1);
+        if (!temp1){
+
+            printf("Error in allocating memory for Symbol_table");
+            printf("\n program will terminate");
+            exit(0);
+
+        }
+        symbol_table = temp1;
+        free(temp1);
+        symbol_table[i+1] = temp;
+    }
     
 }
 
