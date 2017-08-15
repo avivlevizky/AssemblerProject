02893b1c1add8518424f4d6ac3f985af6eeac763
mv9944 <mv9944@gmail.com>
2017-08-15 15:07:50 +0300

    Update base4.c

--- base4.c
+++ base4.c
@@ -1,10 +1,30 @@
-char * base4 (int i) {
+char * base4 (int i, int j) {
     int res;
     int k;
     int f;
+    int length;
     char * temp;
+    char * temp1;
     char * ans ;
     k = 0;
+
+    if (i == 0){
+        ans = (char *)malloc(sizeof(char)*(j+1));
+        if (!ans) {
+            printf("\ncannot allocate memory for base4 function\n");
+            printf("\npreform exit\n");
+            exit(0);}
+        ans[0] = 'a';
+        for(f = 1; f < j; f++){
+            ans[f]='a';
+
+        }
+        ans[f] = '\0';
+        return ans;
+
+
+    }
+
     ans = (char *)malloc(sizeof(char)*(k+1));
     if (!ans) {
         printf("\ncannot allocate memory for base4 function\n");
@@ -13,18 +33,18 @@ char * base4 (int i) {
     }
 
     while (i > 0){
- 	res = i%4;
-    	i=i/4;
+        res = i%4;
+        i=i/4;
 
-	if(res == 0){ans[k++] = 'a';}
+        if(res == 0){ans[k++] = 'a';}
         else if(res == 1){ans[k++] = 'b';}
         else if(res == 2){ans[k++] = 'c';}
-        else if(res == 3){ans[k++] = 'd';}      
+        else if(res == 3){ans[k++] = 'd';}
 
 
         temp = (char*)realloc(ans,(k+1));
         if (!temp) {
-            printf("\ncannot allocate memory for base4 function - k is %d and i is %d\n",k,i);
+            printf("\ncannot allocate memory for base4 function");
             printf("preform exit\n");
             exit(0);
         }
@@ -34,30 +54,47 @@ char * base4 (int i) {
 
         }
 
-     
+
 
     }
     temp = (char*)realloc(ans,(k+1));
-        if (!temp) {
-            printf("\ncannot allocate memory for base4 function - k is %d and i is %d\n",k,i);
-            printf("preform exit\n");
-            exit(0);
-        }
-        else
+    if (!temp) {
+        printf("\ncannot allocate memory for base4 function");
+        printf("preform exit\n");
+        exit(0);
+    }
+    else
+    {
+        ans = temp;
+
+    }
+
+
+    temp = (char *)malloc(sizeof(char)*(k+1));
+    for(f = 0; f < k; f++)
+    {
+        temp[f] = ans[(k-1)-f];
+    }
+    temp[k]='\0';
+    free(ans);
+    ans = temp;
+    length = strlen(ans);
+    if (j > length){
+        temp1 = (char *)malloc(sizeof(char)*(j+1));
+        for (f = 0; f < j; f++)
         {
-            ans = temp;
+            if (f <(j-length))
+            {
+                temp1[f] = 'a';
+            }
+            else
+            {
+                temp1[f] = temp[f-(j-length)];
+            }
 
         }
-
-
-	temp = (char *)malloc(sizeof(char)*(k+1));
-	for(f = 0; f < k; f++)
-	{	
-	temp[f] = ans[(k-1)-f];	
-	}
-	temp[k]='\0';
-	free(ans);
-
-	ans = temp;
+            free(ans);
+            return temp1;
+    }
     return ans;
 }
