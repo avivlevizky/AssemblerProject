1ec85138e7d7b9584045cbedfc1d1cfb13930d55
avivlevizky <avivlevizky@gmail.com>
2017-08-15 18:07:17 +0300

    Update Assembler.c

--- Assembler.c
+++ Assembler.c
@@ -304,70 +304,106 @@ void SecondCheckingCommand(char ** command)
 	}
 }
 
-char * base4(int i) 
-{
-	int res;
-	int k;
-	int f;
-	char * temp;
-	char * ans;
+char * base4 (int i, int j) {
+    int res;
+    int k;
+    int f;
+    int length;
+    char * temp;
+    char * temp1;
+    char * ans ;
+    k = 0;
 
-	k = 0;
-	ans = (char *)malloc(sizeof(char)*(k + 1));
-	if (!ans) 
-	{
-		printf("\ncannot allocate memory for base4 function\n");
-		printf("\npreform exit\n");
-		exit(0);
-	}
+    if (i == 0){
+        ans = (char *)malloc(sizeof(char)*(j+1));
+        if (!ans) {
+            printf("\ncannot allocate memory for base4 function\n");
+            printf("\npreform exit\n");
+            exit(0);}
+        ans[0] = 'a';
+        for(f = 1; f < j; f++){
+            ans[f]='a';
 
-	while (i > 0) 
-	{
-		res = i % 4;
-		i = i / 4;
-
-		if (res == 0) { ans[k++] = 'a'; }
-		else if (res == 1) { ans[k++] = 'b'; }
-		else if (res == 2) { ans[k++] = 'c'; }
-		else if (res == 3) { ans[k++] = 'd'; }
+        }
+        ans[f] = '\0';
+        return ans;
 
 
-		temp = (char*)realloc(ans, (k + 1));
-		if (!temp) {
-			printf("\ncannot allocate memory for base4 function - k is %d and i is %d\n", k, i);
-			printf("preform exit\n");
-			exit(0);
-		}
-		else
-		{
-			ans = temp;
+    }
 
-		}
+    ans = (char *)malloc(sizeof(char)*(k+1));
+    if (!ans) {
+        printf("\ncannot allocate memory for base4 function\n");
+        printf("\npreform exit\n");
+        exit(0);
+    }
+
+    while (i > 0){
+        res = i%4;
+        i=i/4;
+
+        if(res == 0){ans[k++] = 'a';}
+        else if(res == 1){ans[k++] = 'b';}
+        else if(res == 2){ans[k++] = 'c';}
+        else if(res == 3){ans[k++] = 'd';}
+
+
+        temp = (char*)realloc(ans,(k+1));
+        if (!temp) {
+            printf("\ncannot allocate memory for base4 function");
+            printf("preform exit\n");
+            exit(0);
+        }
+        else
+        {
+            ans = temp;
+
+        }
 
 
 
-	}
+    }
+    temp = (char*)realloc(ans,(k+1));
+    if (!temp) {
+        printf("\ncannot allocate memory for base4 function");
+        printf("preform exit\n");
+        exit(0);
+    }
+    else
+    {
+        ans = temp;
 
-	temp = (char*)realloc(ans, (k + 1));
-	if (!temp) 
-	{
-		printf("\ncannot allocate memory for base4 function - k is %d and i is %d\n", k, i);
-		printf("preform exit\n");
-		exit(0);
-	}
-	else
-		ans = temp;
+    }
 
-	temp = (char *)malloc(sizeof(char)*(k + 1));
-	for (f = 0; f < k; f++)
-		temp[f] = ans[(k - 1) - f];
 
-	temp[k] = '\0';
-	free(ans);
-	ans = temp;
-	return ans;
+    temp = (char *)malloc(sizeof(char)*(k+1));
+    for(f = 0; f < k; f++)
+    {
+        temp[f] = ans[(k-1)-f];
+    }
+    temp[k]='\0';
+    free(ans);
+    ans = temp;
+    length = strlen(ans);
+    if (j > length){
+        temp1 = (char *)malloc(sizeof(char)*(j+1));
+        for (f = 0; f < j; f++)
+        {
+            if (f <(j-length))
+            {
+                temp1[f] = 'a';
+            }
+            else
+            {
+                temp1[f] = temp[f-(j-length)];
+            }
+
+        }
+            free(ans);
+            return temp1;
+    }
+    return ans;
 }
-
 
 
 
@@ -461,40 +497,40 @@ int main(int argc,char ** argv) {
 	while (i < IC)
 	{
 		char ans[6] = "";
-		addr = base4(IC + 100);
+		addr = base4(IC,5);
 
 		if ((instructions_table[i]->type_order) == 0)
 		{
 
-			temp = base4(((InstructOrder *)instructions_table[i]->order)->type_coding);
+			temp = base4(((InstructOrder *)instructions_table[i]->order)->type_coding,1);
 			strcat(ans, temp);
 			free(temp);
-			temp = base4(((InstructOrder *)instructions_table[i]->order)->origin_addressing);
+			temp = base4(((InstructOrder *)instructions_table[i]->order)->origin_addressing,1);
 			strcat(ans, temp);
 			free(temp);
-			temp = base4(((InstructOrder *)instructions_table[i]->order)->dest_addressing);
+			temp = base4(((InstructOrder *)instructions_table[i]->order)->dest_addressing,1);
 			strcat(ans, temp);
 			free(temp);
-			temp = base4(((InstructOrder *)instructions_table[i]->order)->opcode);
+			temp = base4(((InstructOrder *)instructions_table[i]->order)->opcode,2);
 			strcat(ans, temp);
 			free(temp);
 		}
 		if ((instructions_table[i]->type_order) == 1)
 		{
-			temp = base4(((InstructData*)instructions_table[i]->order)->type_coding);
+			temp = base4(((InstructData*)instructions_table[i]->order)->type_coding,1);
 			strcat(ans, temp);
 			free(temp);
-			temp = base4(((InstructData*)instructions_table[i]->type_order)->value);
+			temp = base4(((InstructData*)instructions_table[i]->type_order)->value,4);
 			strcat(ans, temp);
 			free(temp);
 		}
 		if ((instructions_table[i]->type_order) == 2)
 		{
 			strcat(ans, "a");
-			temp = base4(((InstructRegisters*)instructions_table[i]->order)->reg2);
+			temp = base4(((InstructRegisters*)instructions_table[i]->order)->reg2,2);
 			strcat(ans, temp);
 			free(temp);
-			temp = base4(((InstructRegisters*)instructions_table[i]->type_order)->reg1);
+			temp = base4(((InstructRegisters*)instructions_table[i]->type_order)->reg1,2);
 			strcat(ans, temp);
 			free(temp);
 		}
