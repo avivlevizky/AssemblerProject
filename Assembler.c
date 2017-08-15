2168274d4401f579648c44546950a9c69d83e16b
avivlevizky <avivlevizky@gmail.com>
2017-08-15 14:19:04 +0300

    Add files via upload

--- Assembler.c
+++ Assembler.c
@@ -304,6 +304,83 @@ void SecondCheckingCommand(char ** command)
 	}
 }
 
+char * base4(int i) 
+{
+	int res;
+	int k;
+	int f;
+	char * temp;
+	char * ans;
+
+	k = 0;
+	ans = (char *)malloc(sizeof(char)*(k + 1));
+	if (!ans) 
+	{
+		printf("\ncannot allocate memory for base4 function\n");
+		printf("\npreform exit\n");
+		exit(0);
+	}
+
+	while (i > 0) 
+	{
+		res = i % 4;
+		i = i / 4;
+
+		if (res == 0) { ans[k++] = 'a'; }
+		else if (res == 1) { ans[k++] = 'b'; }
+		else if (res == 2) { ans[k++] = 'c'; }
+		else if (res == 3) { ans[k++] = 'd'; }
+
+
+		temp = (char*)realloc(ans, (k + 1));
+		if (!temp) {
+			printf("\ncannot allocate memory for base4 function - k is %d and i is %d\n", k, i);
+			printf("preform exit\n");
+			exit(0);
+		}
+		else
+		{
+			ans = temp;
+
+		}
+
+
+
+	}
+
+	temp = (char*)realloc(ans, (k + 1));
+	if (!temp) 
+	{
+		printf("\ncannot allocate memory for base4 function - k is %d and i is %d\n", k, i);
+		printf("preform exit\n");
+		exit(0);
+	}
+	else
+		ans = temp;
+
+	temp = (char *)malloc(sizeof(char)*(k + 1));
+	for (f = 0; f < k; f++)
+		temp[f] = ans[(k - 1) - f];
+
+	temp[k] = '\0';
+	free(ans);
+	ans = temp;
+	return ans;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
